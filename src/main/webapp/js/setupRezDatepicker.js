ecf3ec9ac15010cd26dd6e09078e2b99483b736d
developer9608 <139551730+developer9608@users.noreply.github.com>
2023-10-17 12:56:50 +0900

    Update setupRezDatepicker.js

--- src/main/webapp/js/setupRezDatepicker.js
+++ src/main/webapp/js/setupRezDatepicker.js
@@ -4,10 +4,10 @@
  $(document).ready(function() {
 	$.datepicker.setDefaults({
 		showMonthAfterYear:true
-	    ,showOn: "both"
-	    ,buttonImage: "https://clipart-library.com/img/848153.jpg" //버튼 이미지 경로
-	    ,buttonImageOnly: true //기본 버튼의 회색 부분을 없애고, 이미지만 보이게 함
-        ,buttonText: "선택" //버튼에 마우스 갖다 댔을 때 표시되는 텍스트
+	    	,showOn: "both"
+	    	,buttonImage: "https://clipart-library.com/img/848153.jpg" //버튼 이미지 경로
+	    	,buttonImageOnly: true //기본 버튼의 회색 부분을 없애고, 이미지만 보이게 함
+        	,buttonText: "선택" //버튼에 마우스 갖다 댔을 때 표시되는 텍스트
 		,monthNamesShort: ['1','2','3','4','5','6','7','8','9','10','11','12'] //달력의 월 부분 텍스트
 		,monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'] //달력의 월 부분 Tooltip 텍스트
 		,dayNamesMin: ['일','월','화','수','목','금','토'] //달력의 요일 부분 텍스트
